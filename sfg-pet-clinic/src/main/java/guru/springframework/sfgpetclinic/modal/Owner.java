31f33354b5b69ff400089209dcb31d15623d371c
Mo Taraq <98199343+mavicmo@users.noreply.github.com>
2024-05-07 08:47:43 -0400

    initial commit

---
+++ sfg-pet-clinic/src/main/java/guru/springframework/sfgpetclinic/modal/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinic.modal;
+
+public class Owner extends Person{
+}
---
+++ sfg-pet-clinic/src/main/java/guru/springframework/sfgpetclinic/modal/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinic.modal;
+
+public class Vet extends Person{
+}
